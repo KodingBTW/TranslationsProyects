5e046ad16c4b902ca0ea561523db0bac7c83c3a1
KodingBTW <traduccioneskoda@gmail.com>
2026-06-29 17:04:35 -0400

    translation WIP

--- GBA/Battle B-Daman - Fire Spirits! (USA)/extract/extract_gfx.bat
+++ GBA/Battle B-Daman - Fire Spirits! (USA)/extract/extract_gfx.bat
@@ -1,6 +1,6 @@
 set ROM="../Battle B-Daman - Fire Spirits! (USA).gba"
 
-..\tools\bbd_nybbles.py decompress -r %ROM% -f "main_fonts.bin" -o "CF75C" -s "384"
+..\tools\bbd_nybbles.py decompress -r %ROM% -f "main_fonts.bin" -o "CF75C" -s "6A40"
 ..\tools\bbd_lz_rle.py unpack -a 0x70C488 -o "nintendo_license.bin" %ROM%
 ..\tools\bbd_lz_rle.py unpack -a 0x70E74C -o "title_screen_logo.bin" %ROM%
 ..\tools\bbd_lz_rle.py unpack -a 0x70F341 -o "title_screen_start.bin" %ROM%
